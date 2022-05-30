4f1743826cab77a4eca4c4d9b71e91462aa95cb5
viper-timon <viper-timon@mail.ru>
2022-05-30 17:26:58 +0300

    add Tasks from Seminar

---
+++ Task_40/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три
+// числа и проверяет, может ли существовать треугольник с сторонами
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+Console.Write("Введите 1е число -> ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите 2е число -> ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите 3е число -> ");
+int c = int.Parse(Console.ReadLine());
+
+string txt = "Треугольника с такими сторонами не существует";
+if ((a + b) > c && (a + c) > b && (b + c) > a) txt = "Треугольник с такими сторонами может существовать";
+
+Console.WriteLine(txt);
+
+// int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+// Console.Write($"{array[0]} {array[1]} {array[2]}");
---
+++ Task_42/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите число для перевода");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите систему счисления");
+int y = int.Parse(Console.ReadLine());
+int len = 8;
+int[] result = new int[len];
+int wholePart = x; //целая часть 
+int remainderPart = y; // остаточная часть
+
+for (int i = 0; wholePart > 0; i++)
+{
+    // if (wholePart<=y)
+    // {
+    // result[i] = wholePart;
+    // }
+    // else
+    // {
+    remainderPart = wholePart % y;
+    wholePart = wholePart / y;
+    result[i] = remainderPart;
+
+    Console.WriteLine($"i= {i} whole = {wholePart} rem {remainderPart}");
+}
+result.Reverse();
+
+
+Console.WriteLine($"{String.Join(" ", result)}");
